2805347c72d5c6b1327467b91c486a16b5a80a3a
jsuchanun15 <j.suchana15@gmail.com>
2024-11-29 15:25:50 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,9 +2,20 @@
 using namespace std;
 
 int main(){
+    int x;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    cout << 555
+    cin >> x;
+    int even = 0, odd = 0;
+    while(x != 0){
+        if(x % 2 == 0){
+            even += 1;
+        }else{
+            odd += 1;
+        }
+        cout << "Enter an integer: ";
+        cin >> x;
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
